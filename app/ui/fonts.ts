ba8a55957fe4118aaf3029804486e2d370c40029
Ezra <ezra3sh@gmail.com>
2024-08-22 21:18:39 +0700

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
+export const plusJakartaSans = Plus_Jakarta_Sans({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
